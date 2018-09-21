5dc9f937812ef1280efd765a4318023d0766be9b
gregibogyo <g.albert95@hotmail.com>
2018-09-21 23:04:56 +0200

    Data Converter fixed.

--- Semantic Segmentation Dataset Generator/data_converter.py
+++ Semantic Segmentation Dataset Generator/data_converter.py
@@ -18,24 +18,24 @@ imageset = 'training'
 mapillary_raw_dict = os.path.join(mapillary_dict, 'raw')
 mapillary_512_dict = os.path.join(mapillary_dict, '512')
 
-raw_mapillary_images_dict = os.path.join(mapillary_raw_dict, imageset+"\\images")
-raw_mapillary_labels_dict = os.path.join(mapillary_raw_dict, imageset+'\\labels')
+raw_mapillary_images_dict = os.path.join(mapillary_raw_dict, imageset + "\\images")
+raw_mapillary_labels_dict = os.path.join(mapillary_raw_dict, imageset + '\\labels')
 
-_512_mapillary_images_dict = os.path.join(mapillary_512_dict, imageset+"\\images")
+_512_mapillary_images_dict = os.path.join(mapillary_512_dict, imageset + "\\images")
 if not os.path.exists(_512_mapillary_images_dict):
     os.makedirs(_512_mapillary_images_dict)
 
-_512_mapillary_labels_dict = os.path.join(mapillary_512_dict, imageset+"\\labels")
+_512_mapillary_labels_dict = os.path.join(mapillary_512_dict, imageset + "\\labels")
 if not os.path.exists(_512_mapillary_labels_dict):
     os.makedirs(_512_mapillary_labels_dict)
 
-_512_mapillary_edges_dict = os.path.join(mapillary_512_dict, imageset+"\\edges")
+_512_mapillary_edges_dict = os.path.join(mapillary_512_dict, imageset + "\\edges")
 if not os.path.exists(_512_mapillary_edges_dict):
     os.makedirs(_512_mapillary_edges_dict)
 
-#load the names if exist, if not read the names and save them
-mapillary_names = os.path.join(mapillary_512_dict, "trainingimages.dll")
-if os._exists(mapillary_names):
+# load the names if exist, if not read the names and save them
+mapillary_names = os.path.join(mapillary_512_dict, "images.dll")
+if os._exists(mapillary_names) and not deleteall:
     with open(mapillary_names, 'rb') as f:
         imagenames = pickle.load(f)
 else:
@@ -45,6 +45,7 @@ else:
         pickle.dump(imagenames, f)
 
 print(imagenames)
+_512_imagenames = []
 
 for i in range(len(imagenames)):
     imagename = imagenames[i] + ".jpg"
@@ -65,26 +66,40 @@ for i in range(len(imagenames)):
         raw_labelheight = raw_label.shape[0]
         label_rate = 512 / raw_labelheight
         _512_label = cv2.resize(raw_label, dsize=(0, 0), fx=label_rate, fy=label_rate, interpolation=cv2.INTER_CUBIC)
-        _512_label = _512_label[:, 0:683]
+        _512_label = _512_label[:, 0:640]
         _512_label = np.clip(_512_label, a_min=0, a_max=66)
         cv2.imwrite(filename=_512_label_path, img=_512_label)
 
     if not os.path.exists(_512_edge_path) or deleteall:
         _512_edge = cv2.Canny(_512_label, threshold1=0, threshold2=1)
-        _512_edge = _512_edge[:, 0:683]
+        _512_edge = _512_edge[:, 0:640]
         cv2.imwrite(filename=_512_edge_path, img=_512_edge)
+
 
     if not os.path.exists(_512_image_path) or deleteall:
         raw_imageheight = raw_image.shape[0]
         image_rate = 512 / raw_imageheight
         _512_image = cv2.resize(raw_image, dsize=(0, 0), fx=image_rate, fy=image_rate, interpolation=cv2.INTER_CUBIC)
-        _512_image = _512_image[:, 0:683]
+        _512_image = _512_image[:, 0:640]
         _512_image = cv2.cvtColor(_512_image, cv2.COLOR_RGB2BGR)
         cv2.imwrite(filename=_512_image_path, img=_512_image)
 
+    if _512_label.shape != (512, 640) or _512_edge.shape != (512, 640) or _512_image.shape != (512, 640, 3):
+        if os.path.exists(_512_label_path):
+            os.remove(_512_label_path)
+        if os.path.exists(_512_edge_path):
+            os.remove(_512_edge_path)
+        if os.path.exists(_512_image_path):
+            os.remove(_512_image_path)
+    else:
+        _512_imagenames.append(imagenames[i])
 
-        print("%i.\n "
-              "\t Label: Size: %s \t Min: %s \t Max: %s \n"
-              "\t Edge: Size: %s \n"
-              "\t Image: Size: %s \n"
-              % (i, _512_label.shape, np.min(_512_label), np.max(_512_label), _512_edge.shape, _512_image.shape))
+
+    print("%i.\n "
+          "\t Label: Size: %s \t Min: %s \t Max: %s \n"
+          "\t Edge: Size: %s \n"
+          "\t Image: Size: %s \n"
+          % (i, _512_label.shape, np.min(_512_label), np.max(_512_label), _512_edge.shape, _512_image.shape))
+
+    with open(mapillary_names, 'wb') as f:
+        pickle.dump(_512_imagenames, f)
