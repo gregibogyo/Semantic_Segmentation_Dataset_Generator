d12dd0625440c6a29c6fb50f25efd029053e6ffe
albert <g.albert95@hotmail.com>
2018-10-31 22:54:51 +0000

    top separation and reattachment done

--- Semantic Segmentation Dataset Generator/core/layers/conv_crfrnn_layer.py
+++ Semantic Segmentation Dataset Generator/core/layers/conv_crfrnn_layer.py
@@ -1,3 +1,4 @@
+from keras.backend import tf as tf
 import keras as K
 import numpy as np
 
@@ -18,7 +19,7 @@ class ConvCrfRnnLayer(K.layers.Layer):
         super(ConvCrfRnnLayer, self).__init__(**kwargs)
 
     def build(self, input_shape):
-        self.image_dims = input_shape[1][-1]
+        self.image_channels = input_shape[1][-1]
         self.num_classes = input_shape[0][-1]
 
         # self.contrast_weight_matrix = self.add_weight(name='contrast_weight_matrix',
@@ -45,7 +46,7 @@ class ConvCrfRnnLayer(K.layers.Layer):
         #         K.backend.expand_dims(self.compability_matrix, 0), 0), 0)
 
         self.contrast_difference_kernel = self.contrast_difference_kernel_initalizer(kernel_size=3,
-                                                                                     depth=self.image_dims,
+                                                                                     depth=self.image_channels,
                                                                                      middle_item=-1)
         # self.contrast_theta = self.add_weight(name='contrast_theta',
         #                                       shape=[1],
@@ -70,53 +71,34 @@ class ConvCrfRnnLayer(K.layers.Layer):
                                      shape=([K.backend.shape(contrast)[0],
                                              contrast.get_shape().as_list()[1],
                                              contrast.get_shape().as_list()[2],
-                                             int(contrast.get_shape().as_list()[3] / self.image_dims),
-                                             self.image_dims]))
+                                             int(contrast.get_shape().as_list()[3] / self.image_channels),
+                                             self.image_channels]))
 
         contrast = K.backend.sum(contrast ** 2, axis=-1)
         k = K.backend.exp(- contrast)
 
-        num_batch = K.backend.tf.shape(image)[0]
-        num_height = K.backend.tf.shape(image)[1]
-        num_width = K.backend.tf.shape(image)[2]
-        batch_range = K.backend.tf.range(num_batch)
-        batch_height = K.backend.tf.range(num_height)
-        batch_width = K.backend.tf.range(num_width)
-        row_tensor = K.backend.tf.tile(batch_range[:, None], (1, 1))
+        num_batch = 1
+        num_height = 512
+        num_width = 640
+        batch_range = tf.range(num_batch)
+        height_range = tf.range(num_height)
+        width_range = tf.range(num_width)
+        batch_tensor = tf.tile(batch_range[:, None, None, None], (1, num_height, num_width, 1))
+        height_tensor = tf.tile(height_range[None, :, None, None], (num_batch, 1, num_width, 1))
+        width_tensor = tf.tile(width_range[None, None, :, None], (num_batch, num_height, 1, 1))
 
         for i in range(self.num_iterations):
-            Q, indices = K.backend.tf.nn.top_k(Q_all, k=1)
-            # Q = K.backend.depthwise_conv2d(Q,
-            #                                depthwise_kernel=self.unary_kernel,
-            #                                padding='same')
-            #
-            # Q = K.backend.reshape(Q,
-            #                       shape=([K.backend.shape(Q)[0],
-            #                               K.backend.shape(Q)[1],
-            #                               K.backend.shape(Q)[2],
-            #                               int(Q.get_shape().as_list()[3] / self.top_class_number),
-            #                               self.top_class_number]))
-            #
-            # # Q = K.backend.sum(Q, axis=-2)
-            # # Q = K.backend.sum(K.backend.expand_dims(k, axis=-1) * Q, axis=-2)
-            # Q = K.backend.batch_dot(k, Q, axes=[-1, -2])
-            # Q = K.backend.sum(self.contrast_weight_matrix * K.backend.expand_dims(Q, -1), axis=-2)
-            # Q = K.backend.sum(self.compability_matrix * K.backend.expand_dims(Q, -1), axis=-2)
+            Q, indices = tf.nn.top_k(Q_all, k=1)
 
+            # shape the indices into scatter_nd form
+            batch_height_width_stack = tf.stack([batch_tensor, height_tensor, width_tensor, indices], axis=4)
 
+            # build the mask where there are ones in the indices places
+            Q_augmented = tf.scatter_nd(indices=batch_height_width_stack,
+                                        updates=Q,
+                                        shape=[num_batch, num_height, num_width, self.num_classes])
 
-            # stack along the final dimension, as this is what
-            # scatter_nd uses as the indices
-            top_k_row_col_indices = K.backend.tf.stack([row_tensor, indices], axis=2)
-
-            # to mask off everything, we will multiply the top_k by
-            # 1. so all the updates are just 1
-            updates = K.backend.tf.ones([num_batch, k], dtype=K.backend.tf.float32)
-
-            # build the mask
-            zero_mask = K.backend.tf.scatter_nd(top_k_row_col_indices, updates, [num_batch, 4])
-            shape = K.backend.tf.constant([1, 512, 640, 67])
-            Q_all = K.backend.tf.scatter_nd_add(ref=Q_all, indices=indices, updates=Q)
+            Q_all = Q_all + Q_augmented
             potencial = (self.unary_weight * unaries + self.contrast_weight * Q_all) / \
                         (self.unary_weight + self.contrast_weight)
             Q_all = K.backend.exp(potencial)
